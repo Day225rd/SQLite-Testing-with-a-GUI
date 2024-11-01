5c34372af4862d1e25a787728c650d04f68db760
Day225rd <kylejackson2256@gmail.com>
2024-10-31 20:07:51 -0500

    added more to the button

--- GUI.py
+++ GUI.py
@@ -7,11 +7,20 @@ from tkinter import ttk
 root =Tk()
 class FeedbackApp:
     def __init__(self,master):
-        self.label1 = ttk.label(master,text = "Click here")
-        self.label1.grid()
+        self.label1 = ttk.Label(master,text = "Welcome")
+        self.label1.place(x=25,y=100)
+        self.btn1 = ttk.Button(master, text = "Click Here")
+        self.btn1.config(command = self.buttontext)
+        self.btn1.place(x=20,y=150)
+    def buttontext(self):
+        print("TEST")
+fbapp = FeedbackApp(root)
+root.mainloop()
 
+
+
+#/////////////////////////////
 # button1 = ttk.Button(root)
 # button1.config(text="Click here")
 # button1.grid()
 
-root.mainloop()
