88e9a43f71adce6bc9e3dc666da879bc33ed6db2
Day225rd <kylejackson2256@gmail.com>
2024-10-31 19:26:44 -0500

    IT IS DONE

--- createTable.py
+++ createTable.py
@@ -3,7 +3,7 @@ connection = sqlite3.connect('1database.db')
 cursor = connection.cursor()
 
 cursor.execute('''
-               CREATE TABLE IF NOT EXISTS Feedback
+               CREATE TABLE IF NOT EXISTS feedback
                
                (Name TEXT PRIMARY KEY
                , 
@@ -13,3 +13,9 @@ cursor.execute('''
                , 
                Feedback TEXT)
                ''')
+
+#cursor.execute('''INSERT INTO Feedback VALUES
+#               ('Kyle', '000-000-0000', '@mail.com', 'L database')''' )
+
+cursor.execute("SELECT * FROM feedback")
+print(cursor.fetchall())
